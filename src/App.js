6baa5f658f8950266594bf2b2584be17c522fa80
Vladimir <vltinica@gmail.com>
2024-04-17 20:47:26 -0400

    create components home and featuredInfo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./app.css";
+import Home from "./components/pages/home/Home";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Topbar />
       <div className="container">
         <Sidebar />
-        <div className="others">Other pages</div>
+        <Home />
       </div>
     </div>
   );
---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./home.css";
+import FeaturedInfo from "../../featuredInfo/FeaturedInfo";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+    </div>
+  );
+};
+
+export default Home;
